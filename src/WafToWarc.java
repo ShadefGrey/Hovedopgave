9571ade879fd3a2d931821a5cf8f819eb34359f9
ckha <zpif@hotmail.com>
2016-11-10 13:21:49 +0100

    now writes all metadata. Still not certain about where the data from the start and end of waf file should go

--- src/WafToWarc.java
+++ src/WafToWarc.java
@@ -11,18 +11,17 @@ public class WafToWarc {
 
     //TODO cate nul nul nul, er slutning, start på næste er lige efter
     private int contentLenght;
-    private byte[] contentCutOff;
     private byte[] metaData = new byte[100];
     private int metaCounter = 0;
     private UUID responseId;
+    byte[] warcFile = new byte[2000];
+    int warcFilePointer = 0;
 
     public byte[] readWaf(File srcFile, UUID infoId, String date) {
-        byte[] warcFile = new byte[2000];
-        int warcFilePointer = 0;
+
         int lengthCounter = 0;
         ArrayList<Byte> inputStartList = new ArrayList<>();
         byte[] inputBytes = new byte[1000];
-        contentCutOff = new byte[100];
 
         try (FileInputStream fInputStream = new FileInputStream(srcFile)) {
 
@@ -38,23 +37,21 @@ public class WafToWarc {
             //while loop that goes through the waf file and create warc records from the data
             while ((content = fInputStream.read()) != -1 && lengthCounter < 100000) {
 
-                if(fileLenght == whenDoesItStop){
-                    System.out.println("ITS STOPPING");
-                }
-                whenDoesItStop++;
 
                 if (!start) {
-                    if (contentCutOff.length <= initialCounter) {
-                        contentCutOff = Service.growByteArray(contentCutOff);
-                    }
-                    contentCutOff[initialCounter] = (byte) content;
-
                     if (metaData.length <= metaCounter) {
                         metaData = Service.growByteArray(metaData);
                     }
                     metaData[metaCounter] = (byte) content;
                     metaCounter++;
                 }
+
+                if(fileLenght == whenDoesItStop){
+                    System.out.println("ITS STOPPING");
+                    byte[] lastMeta = metaDataRecord(date);
+                    addToWarcFile(lastMeta);
+                }
+                whenDoesItStop++;
 
                 if (start) {
                     if (inputBytes.length <= lengthCounter) {
@@ -98,7 +95,7 @@ public class WafToWarc {
 
                 inputStartList.add((byte) content);
 
-                //Find the place to start //TODO behøves denne?
+                //Find the place to start
                 if (!start && initialCounter > 9 && inputStartList.get(initialCounter - 1) == 0 && inputStartList.get(initialCounter - 2) == 0 &&
                         inputStartList.get(initialCounter - 3) == 0 && inputStartList.get(initialCounter - 4) == 0 && inputStartList.get(initialCounter - 5) == 'a' &&
                         inputStartList.get(initialCounter - 6) == 't' && inputStartList.get(initialCounter - 7) == 'a' && inputStartList.get(initialCounter - 8) == 'd') {
@@ -115,13 +112,9 @@ public class WafToWarc {
 
                     contentLenght = lengthCounter;
                     byte[] tmpWarcFile = warcRecord(infoId, date, contentArray);
-                    for (int i = 0; i < tmpWarcFile.length; i++) {
-                        if (warcFile.length <= tmpWarcFile.length + warcFilePointer) {
-                            warcFile = Service.growByteArray(warcFile);
-                        }
-                        warcFile[warcFilePointer] = tmpWarcFile[i];
-                        warcFilePointer++;
-                    }
+
+                    addToWarcFile(tmpWarcFile);
+
                     warcFile[warcFilePointer] = '\r';
                     warcFilePointer++;
                     warcFile[warcFilePointer] = '\n';
@@ -132,16 +125,9 @@ public class WafToWarc {
                     warcFilePointer++;
                     //TODO tilføj metadata
                     if (metaCounter > 0) {
-                        byte[] metaData = metaDataRecord();
+                        byte[] metaDataContent = metaDataRecord(date);
 
-                        for (int i = 0; i < metaData.length; i++) {
-                            if (warcFile.length <= metaData.length + warcFilePointer) {
-                                warcFile = Service.growByteArray(warcFile);
-                            }
-                            warcFile[warcFilePointer] = metaData[i];
-                            warcFilePointer++;
-
-                        }
+                        addToWarcFile(metaDataContent);
 
                         warcFile[warcFilePointer] = '\r';
                         warcFilePointer++;
@@ -159,7 +145,6 @@ public class WafToWarc {
                     lengthCounter = 0;
                     initialCounter = 0;
                     inputBytes = new byte[1000];
-                    contentCutOff = new byte[100];
                     contentLenght = 0;
                     metaCounter = 0;
                     metaData = new byte[100];
@@ -181,15 +166,27 @@ public class WafToWarc {
         return warcFileToReturn;
     }
 
+    private void addToWarcFile(byte[] bytesToAdd){
+        for (int i = 0; i < bytesToAdd.length; i++) {
+            if (warcFile.length <= bytesToAdd.length + warcFilePointer) {
+                warcFile = Service.growByteArray(warcFile);
+            }
+            warcFile[warcFilePointer] = bytesToAdd[i];
+            warcFilePointer++;
+
+        }
+    }
+
     //returns a byte[] consisting of a metadata WARC record with the non content data with WARC-Concurrent-To the relevant response WARC record
-    public byte[] metaDataRecord() {
+    public byte[] metaDataRecord(String date) {
 
         byte[] metaHeader = ("WARC/1.0\r\n" +
                 "WARC-Type: metadata\r\n" +
                 "WARC-Concurrent-To: <urn:uuid:" + responseId + ">\r\n" +
+                "WARC-Date: " + date + "\r\n" +
                 "WARC-IP-Address: 0.0.0.0\r\n" +
                 "WARC-Record-ID: <urn:uuid:" + UUID.randomUUID() + ">\r\n" +
-                "Content-Type: application/http;msgtype=response\r\n" +
+                "Content-Type: application/warc-fields\r\n" +
                 "Content-Length: " + metaCounter +
                 "\r\n" +
                 "\r\n").getBytes();
@@ -210,13 +207,13 @@ public class WafToWarc {
         boolean http = false;
         boolean mime = false;
         int i = 0;
-        for (i = 0; i < contentCutOff.length; i++) {
+        for (i = 0; i < metaData.length; i++) {
             if (http) {
-                if (contentCutOff[i] != 0) {
+                if (metaData[i] != 0) {
                     if (stringToReturn[0] == null) { //fills the first space in the string with the relevant char instead of a null
-                        stringToReturn[0] = (char) contentCutOff[i] + "";
+                        stringToReturn[0] = (char) metaData[i] + "";
                     } else {
-                        stringToReturn[0] += (char) contentCutOff[i];
+                        stringToReturn[0] += (char) metaData[i];
                     }
                 } else {
                     http = false;
@@ -224,24 +221,24 @@ public class WafToWarc {
 
             }
 
-            if (i > 2 && contentCutOff[i] == 'l' && contentCutOff[i - 1] == 'r' && contentCutOff[i - 2] == 'u') {
+            if (i > 2 && metaData[i] == 'l' && metaData[i - 1] == 'r' && metaData[i - 2] == 'u') {
                 i = i + 5;
                 http = true;
             }
 
             if (mime) {
-                if (contentCutOff[i] != 0) {
+                if (metaData[i] != 0) {
                     if (stringToReturn[1] == null) {
-                        stringToReturn[1] = (char) contentCutOff[i] + "";
+                        stringToReturn[1] = (char) metaData[i] + "";
                     } else {
-                        stringToReturn[1] += (char) contentCutOff[i];
+                        stringToReturn[1] += (char) metaData[i];
                     }
                 } else {
                     mime = false;
                 }
             }
 
-            if (i >= 3 && !http && contentCutOff[i] == 'e' && contentCutOff[i - 1] == 'm' && contentCutOff[i - 2] == 'i' && contentCutOff[i - 3] == 'm') {
+            if (i >= 3 && !http && metaData[i] == 'e' && metaData[i - 1] == 'm' && metaData[i - 2] == 'i' && metaData[i - 3] == 'm') {
                 i = i + 4;
                 mime = true;
             }
